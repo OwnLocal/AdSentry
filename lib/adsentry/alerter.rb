dda46330f9508eec349f04a9f18c4de074674c80
Justin Litchfield <litch@me.com>
2013-11-02 21:34:27 -0500

    adds a list of events

--- lib/adsentry/alerter.rb
+++ lib/adsentry/alerter.rb
@@ -5,7 +5,11 @@ class Adsentry::Alerter
   end
 
   def self.since_last_event(list_name)
+    Time.now.to_f - $REDIS.lindex("adsentry:#{list_name}", 0).to_f
+  end
 
+  def self.last_members(list_name, set_size = 1000)
+    $REDIS.lrange("adsentry:#{list_name}", 0, set_size)
   end
 
 end
