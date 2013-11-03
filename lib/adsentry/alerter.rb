a00f8d5f87b2fb9792e3b30ab5559bed2632795f
Justin Litchfield <litch@me.com>
2013-11-02 21:27:20 -0500

    add an alerter to record timeseries events

--- lib/adsentry/alerter.rb
+++ lib/adsentry/alerter.rb
@@ -4,4 +4,8 @@ class Adsentry::Alerter
     $REDIS.lpush("adsentry:#{list_name}", Time.now.to_f)
   end
 
+  def self.since_last_event(list_name)
+
+  end
+
 end
--- spec/adsentry/alerter_spec.rb
+++ spec/adsentry/alerter_spec.rb
@@ -5,4 +5,9 @@ describe Adsentry::Alerter do
   it "can return a timeseries event" do
     Adsentry::Alerter.record(:associaton).should == 1
   end
+
+  it "can return the elapsed time since an event" do
+    Adsentry::Alerter.record(:associaton)
+    Adsentry::Alerter.since_last_event(:associaton).should == 1
+  end
 end
